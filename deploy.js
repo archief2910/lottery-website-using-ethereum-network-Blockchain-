461d66711194860594e434cd70f378d66e52f322
archief2910 <128996033+archief2910@users.noreply.github.com>
2023-07-27 22:16:51 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,9 +3,9 @@ const { Web3 } = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'cat test vacant lawn install avoid family winter noodle dismiss brown agent',
+  '',
   // remember to change this to your own phrase!
-  'https://eth-sepolia.g.alchemy.com/v2/Wgbb8JGwMP80niQrkXhzImF6TINQq0HG'
+  ''
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
